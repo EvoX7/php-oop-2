186e196ef5b25b6c82627db10465abe3ee1116ac
EvoX7 <creativesoultouch@gmail.com>
2022-09-05 20:16:39 +0200

    fix - minor fixes

--- classes/guest.php
+++ classes/guest.php
@@ -4,4 +4,6 @@
 class Guest extends User
 {
 
+    
+
 }
--- index.php
+++ index.php
@@ -11,34 +11,36 @@
 <body>
 
 
-    <?php
-    require_once __DIR__ .  "/classes/user.php" ?>
-    <?php
-    require_once __DIR__ .  "/classes/registeredUser.php" ?>
-    <?php
-    require_once __DIR__ .  "/classes/guest.php" ?>
-    <?php
-    require_once __DIR__ .  "/classes/creditCard.php" ?>
-    <?php
-    require_once __DIR__ .  "/classes/product.php" ?>
-    <?php
-    require_once __DIR__ .  "/classes/food.php" ?>
-    <?php 
-    require_once __DIR__ .  "/classes/payment.php" ?>
+<?php
+    require_once __DIR__ .  "/classes/user.php";
+    
+    require_once __DIR__ .  "/classes/registeredUser.php";
+    
+    require_once __DIR__ .  "/classes/guest.php"; 
+    
+    require_once __DIR__ .  "/classes/creditCard.php"; 
+    
+    require_once __DIR__ .  "/classes/product.php";
+    
+    require_once __DIR__ .  "/classes/food.php";
+    
+    require_once __DIR__ .  "/classes/payment.php";
+    ?>
 
 
+
     <?php
 
-    $marcoTogni = new RegisteredUser('Marco', 'Togni', 'marco.togni@gmmail.com', 'gibberishpasswd', 'Via Napoli, 1', '3495612554');
-    $guest = new Guest('Anonymous', 'Guest');
+    $marcoTogni = new RegisteredUser('Marco', 'Togni', 'marco.togni@gmail.com', 'gibberishpasswd', 'Via Napoli, 1', '3495612554');
+    $guest = new Guest('Anonymous', 'Guest',);
 
     $creditCard1 = new CreditCard('Marco', 'Togni', '546844445478', '10/25', '933');
     $creditCard2 = new CreditCard('Anonymous', 'Guest', '39816422454', '01/23', '489');
 
-    $catProduct = new Food('Max Health','Dry','Best nuggets', 3, 'cat', 3.50 . ' Kg', 'Super good', 14.50 . ' €', 'sku45416498','05/23');
-    $dogProduct = new Food('Omega','Cool - Dry','Dog Life', 1, 'dog', 6.50, 'Delicious', 18.50, 'sku489264862', '01/24');
+    $catProduct = new Food('Max Health','Dry','Awesome Salmon', 3, 'cat', 3.50 . ' Kg', 'Super good', 14.50 . ' €', 'sku45416498','05/23');
+    $dogProduct = new Food('Omega','Cool - Dry','Best nuggets', 1, 'dog', 6.50 . ' Kg', 'Delicious', 18.50 . ' €', 'sku489264862', '01/24');
 
-    var_dump($catProduct)
+    var_dump($guest)
 
 
 
